e17241f5abbdafe6a3c06d406e5421d65f683f68
com.AlifKhosyi <alifkhosyi@gmail.com>
2022-01-12 00:07:49 +0800

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Alif Khosyi Rahmatullah on 12/01/22.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
